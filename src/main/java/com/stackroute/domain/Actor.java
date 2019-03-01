38f49ca3cee67f2eb369279f060c0ed8625a96b7
apbajpa1 <aprajithabajpai@gmail.com>
2019-03-01 15:29:52 +0530

    final

--- src/main/java/com/stackroute/domain/Actor.java
+++ src/main/java/com/stackroute/domain/Actor.java
@@ -4,7 +4,16 @@ public class Actor {
     private String name;
     private String gender;
     private int age;
+    public Actor(String name,String gender,int age)
+    {
+        this.name=name;
+        this.gender=gender;
+        this.age=age;
 
+    }
+    public Actor(){
+
+    }
     @Override
     public String toString() {
         return "Actor{" +
--- src/main/java/com/stackroute/domain/Main.java
+++ src/main/java/com/stackroute/domain/Main.java
@@ -1,12 +1,12 @@
 package com.stackroute.domain;
 
 
-import org.springframework.beans.MutablePropertyValues;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.support.GenericBeanDefinition;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+//import org.springframework.beans.MutablePropertyValues;
+//import org.springframework.beans.factory.support.BeanDefinitionReader;
+//import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+//import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+//import org.springframework.beans.factory.support.GenericBeanDefinition;
+//import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -15,36 +15,19 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 
 public class Main{
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         XmlBeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("bean.xml"));
         Movie myBean = (Movie) beanFactory.getBean("mo");
         System.out.println(myBean);
         ApplicationContext object=new ClassPathXmlApplicationContext("bean.xml");
         Movie second=(Movie) object.getBean("mo");
         System.out.println(second);
-        BeanDefinitionRegistry beanDefinitionRegistry=(BeanDefinitionRegistry) object.getAutowireCapableBeanFactory();
-        GenericBeanDefinition definition=new GenericBeanDefinition();
+        ApplicationContext consObj=new ClassPathXmlApplicationContext("bean.xml");
+        Movie third=(Movie) consObj.getBean("cosmo");
+        System.out.println(third);
+        Movie fourth=(Movie) consObj.getBean("cosmo2");
+       System.out.println(fourth);
+       System.out.println(third==fourth);
 
-        definition.setBeanClass(Movie.class);
-
-        definition.setAutowireCandidate(true);
-
-        MutablePropertyValues mutablePropertyValues=new MutablePropertyValues();
-
-        beanDefinitionRegistry.registerBeanDefinition("mo",definition);
-
-        Movie movie1= (Movie)beanFactory.getBean("mo");
-
-        System.out.println(movie1);
-
-        DefaultListableBeanFactory beanFactoryy = new DefaultListableBeanFactory();
-
-        BeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactoryy);
-
-        reader.loadBeanDefinitions(new FileSystemResource("src/main/resources/bean.xml"));
-
-        Movie movie3=(Movie)beanFactory.getBean("mo");
-
-        System.out.println(movie3);
     }
 }
