070fd442852b5c79e7fa797220723261289bbb09
apbajpa1 <aprajithabajpai@gmail.com>
2019-03-02 18:36:58 +0530

    Interface

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,6 +1,14 @@
 package com.stackroute.domain;
 
-public class Movie {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Movie  implements ApplicationContextAware, BeanFactoryAware , BeanNameAware {
+
      private Actor actor1;
      private Actor actor2;
      private Actor actor3;
@@ -25,5 +33,19 @@ public class Movie {
 
     public void setActor(Actor actor) {
         this.actor1 = actor;
+    }
+
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("ApplicationContextAware"+applicationContext);
+
+    }
+
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactoryAware"+ beanFactory);
+    }
+
+    public void setBeanName(String s) {
+        System.out.println("bean name"+ s);
+
     }
 }
